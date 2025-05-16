8b28ee0027a77fa43fed560fc8760fdeaf5d3d13
willnapier <will@willnapier.com>
2025-05-16 10:11:40 +0100

    Add mouse key functionality

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,6 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define CONFIG_ZMK_MOUSE y
+#define CONFIG_ZMK_MOUSE_DEFAULT_MOVE_VAL 1500  // Optional: Adjust cursor movement speed
+#define CONFIG_ZMK_MOUSE_DEFAULT_SCRL_VAL 20    // Optional: Adjust scroll speed
