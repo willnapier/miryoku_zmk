4d6f30b07da22210454c43d40bea5596b06cfb0e
Will Napier <will@willnapier.com>
2025-05-17 20:46:03 +0100

    Fix MIRYOKU_LAYER_MOUSE to use 40 keycodes without devicetree label

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,21 +1,18 @@
 #pragma once
 
-// Mouse movement and scroll speeds
 #define ZMK_MOUSE_DEFAULT_MOVE_VAL 1500
 #define ZMK_MOUSE_DEFAULT_SCRL_VAL 20
 
-// Thumb key behavior mappings
-#define MIRYOKU_MAPPING_LH_THUMB_3 &mo u_mouse
-#define MIRYOKU_MAPPING_RH_THUMB_1 &mkp BTN1
-#define MIRYOKU_MAPPING_RH_THUMB_2 &mkp BTN3
-#define MIRYOKU_MAPPING_RH_THUMB_3 &mkp BTN2
+// Add a momentary key to access the Mouse layer
+#define MIRYOKU_MAPPING_LH_THUMB_3 &mo MOUSE
 
-// Partial real mouse layer (top 3 rows + thumbs) with 40 valid bindings
+// Define the MOUSE layer contents (40 keycodes total)
 #define MIRYOKU_LAYER_MOUSE \
   U_BTN1, U_BTN2, U_BTN3, U_BTN4, U_BTN5, \
-  U_MH_LEFT, U_MH_DOWN, U_MH_UP, U_MH_RIGHT, U_NA, \
-  U_MW_LEFT, U_MW_DOWN, U_MW_UP, U_MW_RIGHT, U_NA, \
-  &mo u_mouse, U_NA, U_NA, &mkp BTN1, &mkp BTN3, &mkp BTN2, U_NA, U_NA, U_NA, U_NA, \
-  U_NA, U_NA, U_NA, U_NA, U_NA, \
-  U_NA, U_NA, U_NA, U_NA, U_NA, \
-  U_NA, U_NA, U_NA, U_NA, U_NA
+  U_NA,   U_MH_LEFT, U_MH_DOWN, U_MH_UP, U_MH_RIGHT, \
+  U_NA,   U_MW_LEFT, U_MW_DOWN, U_MW_UP, U_MW_RIGHT, \
+  U_NA,   U_NA,      U_NA,      U_NA,     U_NA, \
+  U_NA,   U_NA,      U_NA,      U_NA,     U_NA, \
+  U_NA,   U_NA,      U_NA,      U_NA,     U_NA, \
+  U_NA,   U_NA,      U_NA,      U_NA,     U_NA, \
+  U_NA,   U_NA,      U_NA,      U_NA,     U_NA
