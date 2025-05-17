2447b40235419c4801ef63cd4b0f27102fd358c0
Will Napier <will@willnapier.com>
2025-05-17 02:11:08 +0100

    Add mouse_layer.dtsi for new full mouse layer, totem.keymap and custom_config mouse settings

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,6 +1,4 @@
-// Copyright 2021 Manna Harbour
-// https://github.com/manna-harbour/miryoku
-
-#define CONFIG_ZMK_MOUSE y
-#define CONFIG_ZMK_MOUSE_DEFAULT_MOVE_VAL 1400  // Optional: Adjust cursor movement speed
-#define CONFIG_ZMK_MOUSE_DEFAULT_SCRL_VAL 20    // Optional: Adjust scroll speed
+#pragma once
+#define ZMK_MOUSE_DEFAULT_MOVE_VAL 1500
+#define ZMK_MOUSE_DEFAULT_SCRL_VAL 20
+#define MIRYOKU_MAPPING_LH_THUMB_3 &mo U_MOUSE   // your new Mouse layer
