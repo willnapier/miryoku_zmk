dde1a8d8c2d45b27a190f2041dce3e1a79ef8e67
Will Napier <will@willnapier.com>
2025-05-17 16:46:05 +0100

    Fix: Add U_MOUSE to MIRYOKU_LAYER_LIST so mouse layer activates properly

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,4 +1,17 @@
 #pragma once
+
 #define ZMK_MOUSE_DEFAULT_MOVE_VAL 1500
 #define ZMK_MOUSE_DEFAULT_SCRL_VAL 20
-#define MIRYOKU_MAPPING_LH_THUMB_3 &mo U_MOUSE   // your new Mouse layer
+
+#define MIRYOKU_MAPPING_LH_THUMB_3 &mo U_MOUSE  // your new Mouse layer
+
+#define MIRYOKU_LAYER_LIST \
+  U_BASE, \
+  U_NAV, \
+  U_MOUSE
+
+#define MIRYOKU_LAYER_MOUSE \
+U_NA,  U_NA,  U_NA,  U_NA,  U_NA,       U_NA,  U_NA,       U_NA,        U_NA,         U_NA,         U_NA, \
+U_BTN1,U_BTN2,U_BTN3,U_BTN4,U_BTN5,    U_NA,  U_MH_LEFT,  U_MH_DOWN,   U_MH_UP,      U_MH_RIGHT,   U_NA, \
+U_NA,  U_NA,  U_NA,  U_NA,  U_NA,       U_NA,  U_MW_LEFT,  U_MW_DOWN,   U_MW_UP,      U_MW_RIGHT,   U_NA, \
+U_NA,  U_NA,  U_NA,                     U_NA,  U_NA,       U_NA
